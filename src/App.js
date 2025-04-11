7de14450b513775cfe3b5443fe42d41a3c944620
Tashongedzwanashe <emmamufunde99@gmail.com>
2025-04-11 16:40:32 +0200

    We made changes on the profile header, to make sure that it appears only on the home page.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ProfileHeader from "./components/ProfileHeader";
 import Biography from "./components/Biography";
@@ -25,46 +30,59 @@ import Jobs from "./components/Jobs/Jobs";
 import UsefulLinks from "./components/UsefulLinks/UsefulLinks";
 import "./App.css";
 
+// Create a wrapper component to handle the conditional rendering of ProfileHeader
+const AppContent = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+
+  return (
+    <div className="App">
+      <Navbar />
+      {isHomePage && <ProfileHeader />}
+      <main
+        className={`main-content ${!isHomePage ? "no-profile-header" : ""}`}
+      >
+        <Routes>
+          <Route path="/" element={<Biography />} />
+          <Route path="/research" element={<Research />}>
+            <Route index element={<Science />} />
+            <Route path="science" element={<Science />} />
+            <Route path="projects" element={<Projects />} />
+            <Route path="publications" element={<Publications />} />
+            <Route path="collaborations" element={<Collaborations />} />
+          </Route>
+          <Route path="/teaching" element={<Teaching />}>
+            <Route index element={<Courses />} />
+            <Route path="courses" element={<Courses />} />
+            <Route path="student-projects" element={<StudentProjects />} />
+          </Route>
+          <Route path="/team" element={<Team />}>
+            <Route index element={<LabDirector />} />
+            <Route path="lab-director" element={<LabDirector />} />
+            <Route path="phd-students" element={<PhDStudents />} />
+            <Route path="masters-students" element={<MastersStudents />} />
+            <Route path="alumni" element={<Alumni />} />
+            <Route path="assistants" element={<Assistants />} />
+          </Route>
+          <Route path="/news" element={<News />}>
+            <Route index element={<Events />} />
+            <Route path="events" element={<Events />} />
+          </Route>
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/useful-links" element={<UsefulLinks />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
+
+// Main App component
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        <ProfileHeader />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Biography />} />
-            <Route path="/research" element={<Research />}>
-              <Route index element={<Science />} />
-              <Route path="science" element={<Science />} />
-              <Route path="projects" element={<Projects />} />
-              <Route path="publications" element={<Publications />} />
-              <Route path="collaborations" element={<Collaborations />} />
-            </Route>
-            <Route path="/teaching" element={<Teaching />}>
-              <Route index element={<Courses />} />
-              <Route path="courses" element={<Courses />} />
-              <Route path="student-projects" element={<StudentProjects />} />
-            </Route>
-            <Route path="/team" element={<Team />}>
-              <Route index element={<LabDirector />} />
-              <Route path="lab-director" element={<LabDirector />} />
-              <Route path="phd-students" element={<PhDStudents />} />
-              <Route path="masters-students" element={<MastersStudents />} />
-              <Route path="alumni" element={<Alumni />} />
-              <Route path="assistants" element={<Assistants />} />
-            </Route>
-            <Route path="/news" element={<News />}>
-              <Route index element={<Events />} />
-              <Route path="events" element={<Events />} />
-            </Route>
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/jobs" element={<Jobs />} />
-            <Route path="/useful-links" element={<UsefulLinks />} />
-          </Routes>
-        </main>
-      </div>
+      <AppContent />
     </Router>
   );
 }
